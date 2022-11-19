f15809f1f59ac84e44649efe37440a18f7e3dd11
WanderingLemon <80416647+WanderingLemon@users.noreply.github.com>
2022-11-19 17:42:47 -0500

    added comments to clarify a few things

--- src/parsing.rs
+++ src/parsing.rs
@@ -11,12 +11,14 @@ pub struct Args {
     #[arg(short, long, required = false)]
     pub format: Option<u32>,
     
+    // takes in the output format of the file
     #[arg(short, long, required = false)]
     pub output: Option<String>,
 }
 
 pub fn check_format(fmt: Option<u32>) -> u32{
     match fmt {
+        // add conditions for waht it can and can't be from the version dimensions of QR codes
         Some(x) => x,
         None => 21
     }
